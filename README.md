4c0edbf7f12f77eaa3641610e476b369891bcc42
Bart Wijnants <bart.w@sparkcentral.com>
2019-12-25 09:06:00 +0100

    tailwind

---
+++ README.md
@@ -0,0 +1,15 @@
+# no-name-yet
+
+Repo for blog post: https://dev.bartwijnants.be/no-name-yet/
+
+## quickstart
+
+```shell
+nvm use
+npm install
+npm start
+```
+
+## license
+
+This repo is licensed under the [MIT License](LICENSE).
---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+const siteMetadata = {
+  title: `My Website`,
+  siteUrl: `https://my.web.site`,
+  description: `The best website in the world`
+};
+
+module.exports = {
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: siteMetadata.title,
+        short_name: `Short`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `src/images/icon.svg`
+      }
+    }
+  ]
+};
